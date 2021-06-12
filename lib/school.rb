03989b098489a6c56e575e2a280875bc2d6196e2
Paul Hsueh <hsueh.paul@gmail.com>
2021-06-12 11:07:47 -0400

    commit tree 866b10f2d453c4e816cc43c46d48c8de4b010a70

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||= [] #this means roster first key or give it empty array 
+        roster[grade] << student_name #shovel student name into the roster hash 
+    end
+
+    def grade(student_grade) #I'm not understanding this because I didn't go through the fundlementals of accessing array part
+        roster[student_grade]
+    end 
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+          sorted[grade] = students.sort
+        end
+        sorted
+    end
+
+
+
+
+end
